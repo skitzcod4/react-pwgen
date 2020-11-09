cb351be82a3c4360d5ba1558ab943316b6608262
Justin Praßl <justin.prassl@outlook.de>
2020-11-09 18:33:14 +0100

    Copy2Clipboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './App.css'
 
+const cb = require('clipboardy')
+
 const App = () => {
   const [password, setPassword] = useState('Fire Generate!')
   const [strong, setStrong] = useState(false)
@@ -66,10 +68,19 @@ const App = () => {
     setLength(slider.value)
   }
 
+  const copyPassword = () => {
+    let popover = document.getElementById('copy-popover')
+    cb.write(password)
+    popover.classList.add('popover-anim')
+    setTimeout(() => {
+      popover.classList.remove('popover-anim')
+    }, 3000)
+  }
+
   return (
     <div>
       <a href="https://justinprassl.de/" className="website">
-        <i class="fas fa-arrow-left"></i>
+        <i className="fas fa-arrow-left"></i>
         <span>Justin Praßl</span>
       </a>
       <div className="index">
@@ -79,6 +90,12 @@ const App = () => {
           <div className="box-header">
             <h2 className="password">{password}</h2>
             <button className="btn" onClick={generatePassword}>Generate!</button>
+            <button style={{marginLeft: "8px"}} className="btn btn-square" onClick={copyPassword}>
+              <i className="fas fa-copy"></i>
+              <div className="copy-popover" id="copy-popover">
+                <span>Copied to clipboard!</span>
+              </div>
+            </button>
           </div>
           <div className="box-body">
             <div className="strength">
@@ -97,19 +114,19 @@ const App = () => {
             <div className="options">
               <div className="option">
                 <input id="upper" type="checkbox" checked={uppercase} onChange={() => setUppercase(!uppercase)} />
-                <label for="upper">Uppercase?</label>
+                <label htmlFor="upper">Uppercase?</label>
               </div>
               <div className="option">
                 <input id="lower" type="checkbox" checked={lowercase} onChange={() => setLowercase(!lowercase)} />
-                <label for="lower">Lowercase?</label>
+                <label htmlFor="lower">Lowercase?</label>
               </div>
               <div className="option">
                 <input id="special" type="checkbox" checked={special} onChange={() => setSpecial(!special)} />
-                <label for="special">Special Chars?</label>
+                <label htmlFor="special">Special Chars?</label>
               </div>
               <div className="option">
                 <input id="digits" type="checkbox" checked={digits} onChange={() => setDigits(!digits)} />
-                <label for="digits">Digits?</label>
+                <label htmlFor="digits">Digits?</label>
               </div>
             </div>
           </div>
